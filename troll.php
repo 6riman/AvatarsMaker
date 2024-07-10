bb5ee28a48718abf4b8d67ef21a44c8cc3eaa4c7
6riman <114672029+6riman@users.noreply.github.com>
2024-07-10 20:23:31 +0700

    Update troll.php

--- troll.php
+++ troll.php
@@ -1,6 +1,6 @@
 <?php if(condition_to_check_for){ ?>
     <script type="text/javascript">
-        window.open('url_goes_here', '_blank');
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUO0YDQuNC60YDQvtC70Ls%3D', '_blank');
     </script>
     
     <?  } ?>
